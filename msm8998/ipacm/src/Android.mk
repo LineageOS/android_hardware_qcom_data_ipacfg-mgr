8f0e5ab29fc29e7af85f84dc02ee84d212120f5a
Bruno Martins <bgcngm@gmail.com>
2018-10-29 13:48:35 +0000

    ipacfg-mgr: Use generated kernel headers
    Change-Id: Ie33f0b8b7996536a73f660cba5b4700964d8b03f

--- msm8998/ipacm/src/Android.mk
+++ msm8998/ipacm/src/Android.mk
@@ -23,10 +23,7 @@ LOCAL_C_INCLUDES += external/libxml2/include
 LOCAL_C_INCLUDES += external/libnetfilter_conntrack/include
 LOCAL_C_INCLUDES += external/libnfnetlink/include
 
-ifeq ($(TARGET_COMPILE_WITH_MSM_KERNEL),true)
-LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
-LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
-endif
+LOCAL_HEADER_LIBRARIES := generated_kernel_headers
 
 LOCAL_CFLAGS += -DFEATURE_IPA_ANDROID
 LOCAL_CFLAGS += -DFEATURE_IPACM_HAL -Wall -Werror
