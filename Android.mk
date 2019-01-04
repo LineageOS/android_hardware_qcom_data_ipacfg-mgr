4a441974a67bbc729633a4b85b8c07ec2e202512
Michael Bestas <mkbestas@lineageos.org>
2019-01-04 03:00:20 +0200

    ipacfg-mgr: Use project pathmap
    Change-Id: Ia6d69565655067e86e28d4251970ab4f24b5c9db

--- Android.mk
+++ Android.mk
@@ -1,3 +1,5 @@
+ifeq ($(call my-dir),$(call project-path-for,qcom-data-ipa-cfg-mgr))
+
 ifneq ($(TARGET_BOARD_AUTO),true)
 
     LOCAL_PATH := $(call my-dir)
@@ -5,4 +7,6 @@ ifneq ($(TARGET_BOARD_AUTO),true)
     ifneq ($(filter msm8998,$(TARGET_BOARD_PLATFORM)),)
         include $(call first-makefiles-under,$(LOCAL_PATH)/msm8998)
     endif
+endif
+
 endif
