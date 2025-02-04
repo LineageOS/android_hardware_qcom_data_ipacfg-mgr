816edd3c9a15375640cd56cc530981588efef3b7
Michael Bestas <mkbestas@lineageos.org>
2025-02-04 17:15:40 +0200

    Remove no longer used Android.mk guard
    Change-Id: Ia284075607a1ce3d9312c593258d0b1afcd868b2

--- Android.mk
+++
@@ -1,8 +0,0 @@
-ifneq ($(TARGET_BOARD_AUTO),true)
-
-    LOCAL_PATH := $(call my-dir)
-
-    ifneq ($(filter msm8998,$(TARGET_BOARD_PLATFORM)),)
-        include $(call first-makefiles-under,$(LOCAL_PATH)/msm8998)
-    endif
-endif
